be2931aa51fe8d832a478c0bafb2f76394bafbe5
mnmhbbb <mnmhb64@gmail.com>
2021-04-08 04:44:21 +0900

    빌드를 하기 위한 준비

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === 'production';
+    const plugins = [...config.plugins];
+
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      devtool: prod ? 'hidden-source-map' : 'eval',
+      plugins,
+    };
+  },
+};
